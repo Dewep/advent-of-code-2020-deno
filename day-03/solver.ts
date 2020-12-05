49ba204b767ec8059d58ced2f1f1d70fa406c243
Maigret Aurélien <dewep@pandalab.fr>
2020-12-05 19:23:07 +0100

    Day-03 part-2

--- day-03/solver.ts
+++ day-03/solver.ts
@@ -1,18 +1,29 @@
-export default function solver (map: string[]) {
+function countNumberOfTrees (map: string[], offsetX: number, offsetY: number) {
   const sizeX = map[0].length
   const sizeY = map.length
 
   const currentPos = { x: 0, y: 0 }
   let numberOfTrees = 0
 
-  while (currentPos.y + 1 < sizeY) {
-    currentPos.x = (currentPos.x + 3) % sizeX
-    currentPos.y += 1
+  while (currentPos.y + offsetY < sizeY) {
+    currentPos.x = (currentPos.x + offsetX) % sizeX
+    currentPos.y += offsetY
 
     if (map[currentPos.y][currentPos.x] === '#') {
       numberOfTrees += 1
     }
   }
 
-  console.log('Part 1:', numberOfTrees)
+  return numberOfTrees
+}
+
+export default function solver (map: string[]) {
+  const numberOfTreesX1Y1 = countNumberOfTrees(map, 1, 1)
+  const numberOfTreesX3Y1 = countNumberOfTrees(map, 3, 1)
+  const numberOfTreesX5Y1 = countNumberOfTrees(map, 5, 1)
+  const numberOfTreesX7Y1 = countNumberOfTrees(map, 7, 1)
+  const numberOfTreesX1Y2 = countNumberOfTrees(map, 1, 2)
+
+  console.log('Part 1:', numberOfTreesX3Y1)
+  console.log('Part 2:', numberOfTreesX1Y1 * numberOfTreesX3Y1 * numberOfTreesX5Y1 * numberOfTreesX7Y1 * numberOfTreesX1Y2)
 }
